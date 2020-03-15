14d9454e9d83387529606a49fc7be51b58db2c38
Iryna Paliakova <40804746+syirok@users.noreply.github.com>
2020-03-15 12:12:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let patt = [];
+  for (let i in bracketsConfig) {
+    patt[i] = bracketsConfig[i].join('');
+  } console.log(patt);
+  i = 0;
+  let j = 0;
+  while (i<=str.length-2) {
+    j=0;
+      while (j<=patt.length-1) {
+        if (str.slice(i,i+2) == patt[j]) {
+        str = str.replace(patt[j],''); 
+        i=-1;
+        j=0;
+        break;
+      } 
+      j++;
+    }
+    i++;
+  }
+  return str.length !== 0 ? false:true;
 }
